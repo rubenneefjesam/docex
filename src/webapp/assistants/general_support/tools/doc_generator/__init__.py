bc3735e5b8f5709072f931aa4a17b00e03d4fb8f
rubenneefjesam <neefjesrubenam@gmail.com>
2025-09-15 22:11:45 +0000

    5645

--- src/webapp/assistants/general_support/tools/doc_generator/__init__.py
+++ src/webapp/assistants/general_support/tools/doc_generator/__init__.py
@@ -1,1 +1,9 @@
-from .doc_generator import render
+# src/webapp/assistants/general_support/tools/doc_generator/__init__.py
+
+from .doc_generator import app
+
+# Optioneel ook run/main als je die hebt:
+# from .doc_generator import run, main
+
+# Alias zodat call_tool(render) altijd werkt:
+render = app
