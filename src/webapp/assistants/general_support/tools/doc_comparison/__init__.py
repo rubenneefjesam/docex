b266300316ba063126a520f8d4916e17109dd18c
rubenneefjesam <neefjesrubenam@gmail.com>
2025-09-15 22:45:47 +0000

    change11111

--- src/webapp/assistants/general_support/tools/doc_comparison/__init__.py
+++ src/webapp/assistants/general_support/tools/doc_comparison/__init__.py
@@ -1,9 +1,2 @@
-# src/webapp/assistants/general_support/tools/doc_comparison/__init__.py
-
 from .doc_comparison import app
-
-# Als je run() of main() hebt, alias die dan ook:
-# from .doc_comparison import run, main
-
-# Alias zodat call_tool(render) altijd werkt:
-render = app
+__all__ = ["app"]
