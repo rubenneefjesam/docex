ca80c3026658d2d7cbe7af1f1810981121b87672
rubenneefjesam <neefjesrubenam@gmail.com>
2025-09-15 22:13:06 +0000

    xge323

--- src/webapp/assistants/general_support/tools/doc_comparison/__init__.py
+++ src/webapp/assistants/general_support/tools/doc_comparison/__init__.py
@@ -1,1 +1,9 @@
-from .doc_comparison import render
+# src/webapp/assistants/general_support/tools/doc_comparison/__init__.py
+
+from .doc_comparison import app
+
+# Als je run() of main() hebt, alias die dan ook:
+# from .doc_comparison import run, main
+
+# Alias zodat call_tool(render) altijd werkt:
+render = app
