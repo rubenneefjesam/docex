856d1a46372f27dd04ca3451e8532d425651126a
rubenneefjesam <neefjesrubenam@gmail.com>
2025-09-15 00:05:28 +0000

    changes

---
+++ webapp/assistants/contact.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render():
+    st.header("Contact")
+    st.write("Contactpagina")
---
+++ webapp/assistants/info.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render():
+    st.header("Info")
+    st.write("Infopagina")
