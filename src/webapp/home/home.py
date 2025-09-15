952a61000153b60d66610811bc0848cdae92c87b
rubenneefjesam <neefjesrubenam@gmail.com>
2025-09-15 21:23:08 +0000

    hoe89

--- src/webapp/home/home.py
+++ src/webapp/home/home.py
@@ -4,7 +4,7 @@ def render():
     """
     Dit is de eerste pagina. HOME ookwel genoemd.
     """
-    st.header("Info")
+    st.header("Home")
     st.write("""
         Welkom op de Info-pagina van Docgen Suite.  
         Hier vind je achtergrondinformatie, documentatie en links naar resources.
