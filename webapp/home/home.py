e91042eec0b04178f3fdb57e7f27079e099e5486
rubenneefjesam <neefjesrubenam@gmail.com>
2025-09-15 13:23:45 +0000

    home add

---
+++ webapp/home/home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def render():
+    """
+    Dit is de eerste pagina. HOME ookwel genoemd.
+    """
+    st.header("Info")
+    st.write("""
+        Welkom op de Info-pagina van Docgen Suite.  
+        Hier vind je achtergrondinformatie, documentatie en links naar resources.
+    """)
