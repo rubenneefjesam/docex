7857e7bc3b51d3f2257a2abbd9e70e3739e479cd
rubenneefjesam <neefjesrubenam@gmail.com>
2025-09-15 13:30:13 +0000

    testt

--- webapp/home/__init__.py
+++ webapp/home/__init__.py
@@ -5,8 +5,14 @@ TOOLS = ["— Kies tool —"]
 
 import streamlit as st
 
+# webapp/home/__init__.py
+from .home import render
+
 def render(tool=None):
     st.markdown("<h1>Home</h1>", unsafe_allow_html=True)
     st.write("Welkom bij de Document generator-app.")
     if tool and tool != "— Kies tool —":
         st.warning(f"Onverwachte tool geselecteerd: {tool}")
+
+
+
