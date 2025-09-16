652ca668d87736f32825b93ebb2f04d3b2ce4bb1
rubenneefjesam <neefjesrubenam@gmail.com>
2025-09-16 10:29:01 +0000

    exxie

---
+++ src/webapp/assistants/general_support/tools/doc_comparison/__init__.py
@@ -0,0 +1,11 @@
+# als je bestand 'doc_comparison.py' een functie 'app()' heeft:
+from .doc_comparison import app
+__all__ = ["app"]
+
+# OF: als het 'run()' heet:
+# from .doc_comparison import run as app
+# __all__ = ["app"]
+
+# OF: als het 'main()' heet:
+# from .doc_comparison import main as app
+# __all__ = ["app"]
