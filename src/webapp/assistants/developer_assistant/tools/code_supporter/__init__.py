bfd4a222ba683327f65e466da0ee8711be42a5f2
rubenneefjesam <neefjesrubenam@gmail.com>
2025-10-03 07:05:19 +0000

    sdfsdfsdfsdfsdf

--- src/webapp/assistants/developer_assistant/tools/code_supporter/__init__.py
+++ src/webapp/assistants/developer_assistant/tools/code_supporter/__init__.py
@@ -1,6 +1,6 @@
 # webapp/assistants/sustainability_advisor/tools/sustainability_extractor/__init__.py
 
-from .sustainability_extractor import app
+from .code_supporter import app
 
 # Maak een alias zodat registry óók ‘run’ kan importeren
 run = app
