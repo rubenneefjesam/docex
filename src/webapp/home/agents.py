51afdcc9fcf135682e265204f1f966316f6865f3
rubenneefjesam <neefjesrubenam@gmail.com>
2025-10-04 20:53:40 +0000

    agents

---
+++ src/webapp/home/agents.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def render():
+    st.header("Agents")
+    st.write("Agents")
+
+# webapp/assistants/agents.py
+import streamlit as st
+
+def render():
+    """
+    Vanaf hier kun je meerdere agents instellen die diverse acties achter elkaar doen. 
+    """
+    st.header("Contact")
+    st.write("""
+        Je kunt op een andere pagina zelf agents instellen
+    """)
